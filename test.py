c6f189a84368b6cabfccd9649febd6d36f66604d
Denis Sultu <62120705+onidemon@users.noreply.github.com>
2020-03-31 10:55:33 +0300

    test

--- test.py
+++ test.py
@@ -2,18 +2,20 @@ from  logging import info
 
 name = 'denissultu@hotmail.com'
 
-if '@' in name:
-    for i in name:
-        if i == '@':
-            start = (name.index(i)+1)
+for i in range(len(name)):
+    print(i)
+# if '@' in name:
+#     for i in name:
+#         if i == '@':
+#             start = (name.index(i)+1)
 
 
         
-domain = name[start::]
-print('your domain is {}'.format(domain))
-print('your username is  ',name[:name.index('@'):])
-print('your domain is ',name[name.index('@')+1::])
-d = dict()
+# domain = name[start::]
+# print('your domain is {}'.format(domain))
+# print('your username is  ',name[:name.index('@'):])
+# print('your domain is ',name[name.index('@')+1::])
+# d = dict()
 
-d[name[:name.index('@'):]] =  name[name.index('@')+1::]
-print(d)
+# d[name[:name.index('@'):]] =  name[name.index('@')+1::]
+# print(d)
