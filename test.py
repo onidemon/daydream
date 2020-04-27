b9d9663ae40487190384d4f5926e60e526ce47bc
onidemon <62120705+onidemon@users.noreply.github.com>
2020-04-27 17:07:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -33,7 +33,8 @@ class G2SmartBot:
             }
         self.headers = {
             "Host": "www.g2smart.com",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:76.0) Gecko/20100101 Firefox/76.0",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" 
+            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36",
             "Accept": "application/json, text/plain, */*",
             "Accept-Language": "en-US,en;q=0.5",
             "Accept-Encoding": "gzip, deflate, br",
